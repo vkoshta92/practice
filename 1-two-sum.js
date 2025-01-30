0561292c821287579e9557b8bd1c3b94806910fe
vkoshta92 <114298415+vkoshta92@users.noreply.github.com>
2025-01-30 10:56:19 +0530

    all

---
+++ 1-two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+  const myObject = {};
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (myObject.hasOwnProperty(complement)) {
+      return [myObject[complement], i];
+    }
+    myObject[nums[i]] = i;
+  }
+};
+
+twoSum(5,9)
